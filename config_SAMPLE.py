43ab85a4c5b546ea3ba33e3c2f33ab4f9ef48fdc
Cort Buffington <n0mjs@me.com>
2019-03-02 13:31:14 -0600

    Update ID file format to JSON

--- config_SAMPLE.py
+++ config_SAMPLE.py
@@ -8,14 +8,14 @@ WEB_SERVER_PORT = 8080                  # Has to be above 1024 if you're not run
 
 # Files and stuff for loading alias files for mapping numbers to names
 PATH            = './'                          # MUST END IN '/'
-PEER_FILE       = 'peer_ids.csv'                # Will auto-download from DMR-MARC
-SUBSCRIBER_FILE = 'subscriber_ids.csv'          # Will auto-download from DMR-MARC
-TGID_FILE       = 'talkgroup_ids.csv'           # User provided, should be in "integer TGID, TGID name" format
-LOCAL_SUB_FILE  = 'local_subscriber_ids.csv'    # User provided (optional, leave '' if you don't use it), follow the format of DMR-MARC
-LOCAL_PEER_FILE = 'local_peer_ids.csv'          # User provided (optional, leave '' if you don't use it), follow the format of DMR-MARC
+PEER_FILE       = 'peer_ids.json'                # Will auto-download from DMR-MARC
+SUBSCRIBER_FILE = 'subscriber_ids.json'          # Will auto-download from DMR-MARC
+TGID_FILE       = 'talkgroup_ids.json'           # User provided, should be in "integer TGID, TGID name" format
+LOCAL_SUB_FILE  = 'local_subscriber_ids.json'    # User provided (optional, leave '' if you don't use it), follow the format of DMR-MARC
+LOCAL_PEER_FILE = 'local_peer_ids.json'          # User provided (optional, leave '' if you don't use it), follow the format of DMR-MARC
 FILE_RELOAD     = 7                             # Number of days before we reload DMR-MARC database files
-PEER_URL        = 'https://www.radioid.net/static/rptrs.csv'
-SUBSCRIBER_URL  = 'https://www.radioid.net/static/users.csv'
+PEER_URL        = 'https://www.radioid.net/api/dmr/repeater/?country=united%20states'
+SUBSCRIBER_URL  = 'https://www.radioid.net/api/dmr/user/?country=united%20states'
 # Settings for log files
 LOG_PATH        = PATH
 LOG_NAME        = 'webtables.log'
